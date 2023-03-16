39377e7b9396fea091fef6c937fdf66f611b1574
jose113df <josemateusti@gmail.com>
2023-03-16 11:18:45 -0300

    Entrega projeto parceria

---
+++ projetoparceria/Controllers/CiscoControler.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class CiscoController:Controller{
+
+    public IActionResult Index(){
+        return View();
+    }
+
+}
---
+++ projetoparceria/Controllers/HuaweiControler.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+public class HuaweiController:Controller{
+
+    public IActionResult Index(){
+        return View();
+    }
+
+}
